aa72d4c0c8c413a9f04becb63bfe2dc91796ffc4
vitaliy-ce <ce.vitaliy@ya.ru>
2021-06-12 15:26:28 +0500

    Реализован простейший редактор полученных статей

---
+++ xml-redactor.php
@@ -0,0 +1,64 @@
+<?php
+
+// Загрузка файла
+$file = __DIR__.'/results/result.xml';
+if (!file_exists($file)) {
+    echo '<h1>Файл "'.$file.'" не существует</h1>';
+    exit();
+}
+
+$xml = simplexml_load_file($file, 'SimpleXMLElement', LIBXML_NOCDATA);
+
+// Сохранение результата
+if (isset($_POST['article_index'])) {
+    $xml->article[ (int)$_POST['article_index'] ]->text = $_POST['text'];
+    $xml->asXml($file);
+
+    $alert = 'Статья сохранена';
+}
+
+// Отображаемая статья
+$article_index = (int)$_GET['article_index'] ? (int)$_GET['article_index'] : 0;
+
+
+?>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+<!-- include summernote css/js -->
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+
+
+<div class="container mt-5">
+    <?php if (!empty($alert)) { ?>
+        <div class="alert alert-success" role="alert"><?=$alert?></div>
+    <?php } ?>
+
+    <div class="mb-5">
+        <h1>Статья #<?=$article_index?> — <?=$xml->article[ $article_index ]->name ?? ''?></h1>
+        <a href="/xml-redactor.php?article_index=<?=$article_index - 1?>" class="btn btn-secondary">Назад</a>
+        <a href="/xml-redactor.php?article_index=<?=$article_index + 1?>" class="btn btn-secondary">Вперед</a>
+    </div> 
+
+    <form action="<?=$_SERVER['REQUEST_URI']?>" method="post">
+        <input type="hidden" name="article_index" value="<?=$article_index?>">
+        <div class="mb-3">
+            <textarea class="form-control summernote" name="text" rows="10"><?=$xml->article[ $article_index ]->text ?? ''?></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Сохранить</button>
+    </form>
+</div>
+
+<script>
+    $(document).ready(function(){
+        $(document).ready(function() {
+            $('.summernote').summernote({
+                height: 600
+            });
+        });
+    })
+</script>
